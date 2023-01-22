c1298ba4c97bf2583d1d858b1af7fc88bec7f626
Olyezha <Oleg2pl27@gmail.com>
2023-01-22 22:06:40 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const height = document.querySelector("#height");
+const bottomHeight = document.querySelector("#bottom-height");
+const width = document.querySelector("#width");
+const price = document.querySelector("#price");
+const submit = document.querySelector("#submit");
+const res = document.querySelector(".result")
+
+// let createPrice = document.createElement("span")
+
+let result = 0;
+let calcPrice = 0;
+
+submit.onclick = function(e) {
+    e.preventDefault();
+    result = +width.value * +bottomHeight.value + (+height.value - 0.595 - +bottomHeight.value) * (+width.value - 0.6) + (+height.value - +bottomHeight.value - 0.75) * 0.6;
+    calcPrice = result * +price.value
+    console.log(result);
+    console.log(calcPrice);
+
+    res.innerHTML += `<span> Площа фасадів: ${Math.round(result * 1000) / 1000} </span>`;
+    res.innerHTML += `<span> Ціна: ${Math.ceil(calcPrice)} </span>`;
+
+}
